01fad4d537112b662fd4249520236bd81e50f862
justin-jones <jjone226@odu.edu>
2023-01-25 19:31:21 -0500

    new

--- Assignment1/Inventory.cpp
+++ Assignment1/Inventory.cpp
@@ -50,18 +50,17 @@ Inventory::Inventory(const Inventory &src)
   
    
 
-    // Important - every constructor must initialize
-    // every private data member (attribute)
+    
     this->head  = nullptr;
     this->tail  = nullptr;
-    // this->nodes = 0;
+    
     this->occupied = src.occupied;
     this->slots = src.slots;
-    // Start the Copy Operations
+    
     Node* srcIt = src.head;
    
     while (srcIt != nullptr) {
-      //  this->appendNode(srcIt->data);
+      
       Node *newNode = new Node(srcIt->data);
           if (head == nullptr) {
                 head = newNode;
@@ -78,34 +77,31 @@ Inventory::Inventory(const Inventory &src)
 Inventory::~Inventory()
 {
     // @todo implement this function
- Node *this_iterator = nullptr;  // Loop control pointer
-    Node *to_delete     = nullptr;  // Node to delete
+ Node *this_iterator = nullptr;  
+    Node *to_delete     = nullptr;  
 
-    // start at the beginning of the this
+    
     this_iterator = this->head;
 
-    // iterate through the this and delete each node
+    
     while (this_iterator != nullptr) {
         to_delete = this_iterator;
 
-        // move to next node
+      
         this_iterator = this_iterator->next;
 
-        // delete the current node
+       
         delete to_delete;
 
-        to_delete = nullptr;  // dangling pointers are bad
+        to_delete = nullptr;  
 
-        // Such output would not be included in
-        // a non-academic exercise
-      //  std::cerr << "Deleting Node" << "\n";
+        
     }
 
-    // Such output would not be included in
-    // a non-academic exercise
+   
     
 
-    // Are these three lines necessary?
+    
     head  = nullptr;
     tail  = nullptr;
     
@@ -163,7 +159,7 @@ Inventory::Node* Inventory::findMatchingItemStackNode(const ItemStack& itemStack
      Node* it = head;
 
     while(it != nullptr){
-        // If it and itemStack contain the same type of Item
+        
         if(it->data == itemStack) {
                 
                 return it;
@@ -172,7 +168,7 @@ Inventory::Node* Inventory::findMatchingItemStackNode(const ItemStack& itemStack
         it = it->next;
     }
 
-    // No match was found
+   
     return nullptr;
     
 }
@@ -188,33 +184,28 @@ void Inventory::mergeStacks(ItemStack& lhs, const ItemStack& rhs)
 void Inventory::addItemStackNoCheck(ItemStack itemStack)
 {
     // @todo implement this function
-    // Create a new Node
-    // Note what actually occurs on the next line
+   
     Node *new_node = nullptr;
 
-    // Store the "to_add" data within the node
+    
     new_node = new Node(itemStack);
 
-    // Handle the case where the first node is added
+    
     if (this->occupied == 0) {
-        // this->head = this->tail = new_node;
-        // Note the above line is it any different
-        // from the next two lines?
+        
         this->head = new_node;
         this->tail = new_node;
     }
     else{
-        // Add the new node to the this
-        // What happens on the following two lines
+        
         (this->tail)->next = new_node;
         this->tail = new_node;
     }
 
-    // Increase the number of nodes
+    
     this->occupied++;
 
-    // Do not allow access to the node except
-    // through the linked list
-    // Is this line necessary?
+    
+  
     new_node = nullptr;
 }
